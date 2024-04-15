2fa72b5dd1f9d8f8ff08c7a16d9b60e3b49c5c75
Roner8iny <ronymohamed2323@gmail.com>
2024-04-15 23:16:32 +0200

    adding visualization and more pre processing

--- project.py
+++ project.py
@@ -3,8 +3,10 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.impute import SimpleImputer
 
 ram_categories = ['4 GB', '8 GB', '16 GB', '32 GB']
@@ -13,8 +15,10 @@ hdd_categories = ['0 GB', '512 GB', '1024 GB', '2048 GB']
 graphic_card_gb_categories = ['0 GB', '2 GB', '4 GB', '6 GB', '8 GB']
 warranty_categories = ['No warranty', '1 year', '3 years', '2 years']
 generation_categories = ['4th', '7th', '8th', '9th', '10th', '11th', '12th']
+rating_categories = ['1 star', '2 stars', '3 stars', '4 stars', '5 stars']
 
 label_encoder = LabelEncoder()
+scaler = StandardScaler()
 
 ordinal_encoder_ram = OrdinalEncoder(categories=[ram_categories])
 ordinal_encoder_ssd = OrdinalEncoder(categories=[ssd_categories])
@@ -22,6 +26,7 @@ ordinal_encoder_hdd = OrdinalEncoder(categories=[hdd_categories])
 ordinal_encoder_graphic_card_gb = OrdinalEncoder(categories=[graphic_card_gb_categories])
 ordinal_encoder_warranty = OrdinalEncoder(categories=[warranty_categories])
 ordinal_encoder_generation = OrdinalEncoder(categories=[generation_categories])
+ordinal_encoder_rating = OrdinalEncoder(categories=[rating_categories])
 
 data = pd.read_csv("ElecDeviceRatingPrediction.csv")
 new_data = data
@@ -40,7 +45,7 @@ for column in new_data.columns:
     unique_values = new_data[column].unique()
     print("Unique values in column '{}': {}".format(column, unique_values))
 
-# ram-gb ssd hdd graphic-card warranty generation --> Label Encoding
+# ram-gb ssd hdd graphic-card warranty generation --> Ordinal Encoding
 
 new_data['ram_gb'] = ordinal_encoder_ram.fit_transform(new_data[['ram_gb']])
 new_data['ssd'] = ordinal_encoder_ssd.fit_transform(new_data[['ssd']])
@@ -48,6 +53,7 @@ new_data['hdd'] = ordinal_encoder_hdd.fit_transform(new_data[['hdd']])
 new_data['graphic_card_gb'] = ordinal_encoder_graphic_card_gb.fit_transform(new_data[['graphic_card_gb']])
 new_data['warranty'] = ordinal_encoder_warranty.fit_transform(new_data[['warranty']])
 new_data['processor_gnrtn'] = ordinal_encoder_generation.fit_transform(new_data[['processor_gnrtn']])
+new_data['rating'] = ordinal_encoder_rating.fit_transform(new_data[['rating']])
 
 # From float to int64
 new_data['ram_gb'] = new_data['ram_gb'].astype('int64')
@@ -56,6 +62,7 @@ new_data['hdd'] = new_data['hdd'].astype('int64')
 new_data['graphic_card_gb'] = new_data['graphic_card_gb'].astype('int64')
 new_data['warranty'] = new_data['warranty'].astype('int64')
 new_data['processor_gnrtn'] = new_data['processor_gnrtn'].astype('int64')
+new_data['rating'] = new_data['rating'].astype('int64')
 
 # Touch msoffice weight --> One Hot Encoding
 new_data = pd.get_dummies(new_data, columns=['Touchscreen'])
@@ -77,13 +84,58 @@ new_data['weight_ThinNlight'] = new_data['weight_ThinNlight'].map({True: 1, Fals
 new_data.drop(columns=['msoffice_No'], inplace=True)
 new_data.drop(columns=['Touchscreen_No'], inplace=True)
 
+# Brand , Processor brand, processor name, ram type, os --> Label Encoding
+new_data['brand'] = label_encoder.fit_transform(new_data['brand'])
+new_data['processor_brand'] = label_encoder.fit_transform(new_data['processor_brand'])
+new_data['processor_name'] = label_encoder.fit_transform(new_data['processor_name'])
+new_data['ram_type'] = label_encoder.fit_transform(new_data['ram_type'])
+new_data['os'] = label_encoder.fit_transform(new_data['os'])
+
 # Rating  --> Remove eccess string and convert to int
+
 
 for column in new_data.columns:
     unique_values = new_data[column].unique()
     print("Unique values in column '{}': {}".format(column, unique_values))
 
 new_data.to_csv('output.csv', index=False)
+
+# Scaling
+scaled_data = scaler.fit_transform(new_data)
+scaled_df = pd.DataFrame(scaled_data, columns=new_data.columns)
+
+# Visualization
+# target_column = 'rating'
+# for column in scaled_df:
+#     if column != target_column:
+#         plt.figure(figsize=(8, 6))
+#         sns.scatterplot(x=scaled_df[column], y=scaled_df[target_column])
+#         plt.xlabel(column)
+#         plt.ylabel(target_column)
+#         plt.title(f'Scatter Plot: {column} vs {target_column}')
+#         plt.show()
+
+# median_line_color = 'red'
+# for column in new_data:
+#     if column != target_column:
+#         plt.figure(figsize=(8, 6))
+#         sns.boxplot(x=new_data[column], y=new_data[target_column], medianprops={'color': median_line_color})
+#         plt.xlabel(column)
+#         plt.ylabel(target_column)
+#         plt.title(f'Box Plot: {target_column} vs {column}')
+#
+#         plt.show()
 # print(new_data.info())
 # print(new_data.describe())
 # Outliers
+
+# Feature Selection
+corr_data = new_data.iloc[:, :]
+corr = corr_data.corr()
+top_feature = corr.index[abs((corr['rating']) > 0.1)]
+# Correlation plot
+plt.subplots(figsize=(12, 8))
+top_corr = corr_data[top_feature].corr()
+sns.heatmap(top_corr, annot=True)
+plt.show()
+# top_feature = top_feature.drop('Performance Index')
