a85f7d75e9ea1f46432d720cc399b346c02a5ac5
Roner8iny <ronymohamed2323@gmail.com>
2024-04-20 21:48:03 +0200

    Last Update ISA

--- project.py
+++ project.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn import linear_model
-from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
 from mlxtend.feature_selection import SequentialFeatureSelector
 from sklearn.preprocessing import StandardScaler
@@ -12,7 +11,9 @@ from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import seaborn as sns
-from sklearn.impute import SimpleImputer
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
 
 ram_categories = ['4 GB', '8 GB', '16 GB', '32 GB']
 ssd_categories = ['0 GB', '128 GB', '256 GB', '512 GB', '1024 GB', '2048 GB', '3072 GB']
@@ -35,20 +36,12 @@ ordinal_encoder_rating = OrdinalEncoder(categories=[rating_categories])
 
 data = pd.read_csv("ElecDeviceRatingPrediction.csv")
 new_data = data
-# print(data.isna().sum())
-
-# for column in data.columns:
-#     unique_values = data[column].unique()
-#     print("Unique values in column '{}': {}".format(column, unique_values))
 
 # Replacing Not Available in generation column
 new_data['processor_gnrtn'] = new_data['processor_gnrtn'].replace("Not Available", pd.NA)
 mode_value = new_data['processor_gnrtn'].mode()[0]
 new_data['processor_gnrtn'] = new_data['processor_gnrtn'].fillna(mode_value)
 
-# for column in new_data.columns:
-#     unique_values = new_data[column].unique()
-#     print("Unique values in column '{}': {}".format(column, unique_values))
 
 # ram-gb ssd hdd graphic-card warranty generation --> Ordinal Encoding
 
@@ -73,7 +66,7 @@ new_data['rating'] = new_data['rating'].astype('int64')
 new_data = pd.get_dummies(new_data, columns=['Touchscreen'])
 new_data = pd.get_dummies(new_data, columns=['msoffice'])
 new_data = pd.get_dummies(new_data, columns=['weight'])
-# print(data.describe())
+
 
 new_data['Touchscreen_No'] = new_data['Touchscreen_No'].map({True: 1, False: 0})
 new_data['Touchscreen_Yes'] = new_data['Touchscreen_Yes'].map({True: 1, False: 0})
@@ -85,7 +78,6 @@ new_data['weight_Casual'] = new_data['weight_Casual'].map({True: 1, False: 0})
 new_data['weight_Gaming'] = new_data['weight_Gaming'].map({True: 1, False: 0})
 new_data['weight_ThinNlight'] = new_data['weight_ThinNlight'].map({True: 1, False: 0})
 
-# print(data.info())
 new_data.drop(columns=['msoffice_No'], inplace=True)
 new_data.drop(columns=['Touchscreen_No'], inplace=True)
 
@@ -96,17 +88,13 @@ new_data['processor_name'] = label_encoder.fit_transform(new_data['processor_nam
 new_data['ram_type'] = label_encoder.fit_transform(new_data['ram_type'])
 new_data['os'] = label_encoder.fit_transform(new_data['os'])
 
-# Rating  --> Remove eccess string and convert to int
-
-# for column in new_data.columns:
-#     unique_values = new_data[column].unique()
-#     print("Unique values in column '{}': {}".format(column, unique_values))
 
 # Scaling
 scaled_data = scaler.fit_transform(new_data)
 scaled_df = pd.DataFrame(scaled_data, columns=new_data.columns)
 
-scaled_df.to_csv('output.csv', index=False)
+# scaled_df.to_csv('output.csv', index=False)
+
 # Visualization
 # target_column = 'rating'
 # for column in scaled_df:
@@ -117,7 +105,7 @@ scaled_df.to_csv('output.csv', index=False)
 #         plt.ylabel(target_column)
 #         plt.title(f'Scatter Plot: {column} vs {target_column}')
 #         plt.show()
-
+#
 # median_line_color = 'red'
 # for column in new_data:
 #     if column != target_column:
@@ -128,25 +116,55 @@ scaled_df.to_csv('output.csv', index=False)
 #         plt.title(f'Box Plot: {target_column} vs {column}')
 #
 #         plt.show()
-# print(new_data.info())
-# print(new_data.describe())
-# Outliers
 
 # Feature Selection
+corr_data = scaled_df.iloc[:, :]
+corr = corr_data.corr()
+top_feature = corr.index[abs((corr['rating']) > 0.1)]
+# Correlation plot
+plt.subplots(figsize=(12, 8))
+top_corr = corr_data[top_feature].corr()
+sns.heatmap(top_corr, annot=True)
+plt.show()
 
-# corr_data = new_data.iloc[:, :]
-# corr = corr_data.corr()
-# top_feature = corr.index[abs((corr['rating']) > 0.1)]
-# # Correlation plot
-# plt.subplots(figsize=(12, 8))
-# top_corr = corr_data[top_feature].corr()
-# sns.heatmap(top_corr, annot=True)
-# plt.show()
-# top_feature = top_feature.drop('Performance Index')
+x_top_features = top_feature.drop('rating')
+x_corr = scaled_df[x_top_features]
+
+# Correlation Linear Regression
+y_scaled_corr = scaled_df['rating']
+X_train_corr, X_test_corr, y_train_corr, y_test_corr = train_test_split(x_corr, y_scaled_corr, test_size=0.20, shuffle=True, random_state=10)
+
+linear_model_corr = linear_model.LinearRegression()
+
+linear_model_corr.fit(X_train_corr, y_train_corr)
+y_corr_train_predicted = linear_model_corr.predict(X_train_corr)
+
+y_predict_corr_test = linear_model_corr.predict(X_test_corr)
+
+print('Mean Square Error Correlation Train', metrics.mean_squared_error(np.asarray(y_train_corr), y_corr_train_predicted))
+print('Mean Square Error Correlation Test', metrics.mean_squared_error(np.asarray(y_test_corr), y_predict_corr_test))
+
+# Correlation Polynomial Regression
+poly_features_corr = PolynomialFeatures(degree=3)
+
+X_train_poly_corr = poly_features_corr.fit_transform(X_train_corr)
+
+poly_model_corr = linear_model.LinearRegression()
+
+poly_model_corr.fit(X_train_poly_corr, y_train_corr)
+
+y_train_predicted_corr = poly_model_corr.predict(X_train_poly_corr)
+
+y_predict_test_corr = poly_model_corr.predict(
+    poly_features_corr.transform(X_test_corr))
+
+print('Mean Square Error Corr Train Poly',
+      metrics.mean_squared_error(np.asarray(y_train_corr), y_train_predicted_corr))
+
+print('Mean Square Error Corr Test Poly', metrics.mean_squared_error(np.asarray(y_test_corr), y_predict_test_corr))
 
 y_scaled = scaled_df['rating']
 x_scaled = scaled_df.drop(columns=['rating'])
-# print(x_scaled.columns)
 
 # Train Test Split
 X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=0.20, shuffle=True, random_state=10)
@@ -176,7 +194,7 @@ for column in selected_features_forward_train:
     x_after_feature_selection_forward_train[column_name] = column_values_train
     x_after_feature_selection_forward_test[column_name] = column_values_test
 
-x_after_feature_selection_forward_test.to_csv('forward.csv', index=False)
+# x_after_feature_selection_forward_test.to_csv('forward.csv', index=False)
 
 for column in selected_features_backward_train:
     column_names_backward = x_scaled.columns[column]
@@ -186,18 +204,7 @@ for column in selected_features_backward_train:
     x_after_feature_selection_backward_train[column_name] = column_values_train
     x_after_feature_selection_backward_test[column_name] = column_values_test
 
-x_after_feature_selection_backward_test.to_csv('backward.csv', index=False)
-
-# Test
-# for column in selected_features_forward_test:
-#     column_names_forward = x_scaled.columns[column]
-#     print("Column names selected by forward selection:", column_names_forward)
-#     x_after_feature_selection_forward_test[column_names_forward] = x_scaled[column]
-#
-# for column in selected_features_backward_test:
-#     column_names_backward = x_scaled.columns[column]
-#     print("Column names selected by backward selection:", column_names_backward)
-#     x_after_feature_selection_backward_test[column_names_backward] = x_scaled[column]
+# x_after_feature_selection_backward_test.to_csv('backward.csv', index=False)
 
 
 linear_model_forward = linear_model.LinearRegression()
@@ -213,12 +220,11 @@ y_backward_train_predicted = linear_model_backward.predict(x_after_feature_selec
 
 y_predict_backward_test = linear_model_backward.predict(x_after_feature_selection_backward_test)
 
-print('Mean Square Error Forward Train', metrics.mean_squared_error(np.asarray(y_train), y_forward_train_predicted))
-print('Mean Square Error Forward Test', metrics.mean_squared_error(np.asarray(y_test), y_predict_forward_test))
+print('Linear Regression Mean Square Error Forward Train', metrics.mean_squared_error(np.asarray(y_train), y_forward_train_predicted))
+print('Linear Regression Mean Square Error Forward Test', metrics.mean_squared_error(np.asarray(y_test), y_predict_forward_test))
 
-print('Mean Square Error Backward Train', metrics.mean_squared_error(np.asarray(y_train), y_backward_train_predicted))
-print('Mean Square Error Backward Test', metrics.mean_squared_error(np.asarray(y_test), y_predict_backward_test))
-
+print('Linear Regression Mean Square Error Backward Train', metrics.mean_squared_error(np.asarray(y_train), y_backward_train_predicted))
+print('Linear Regression Mean Square Error Backward Test', metrics.mean_squared_error(np.asarray(y_test), y_predict_backward_test))
 
 poly_features_forward = PolynomialFeatures(degree=2)
 poly_features_backward = PolynomialFeatures(degree=2)
@@ -240,12 +246,63 @@ y_predict_test_forward = poly_model_forward.predict(
 y_predict_test_backward = poly_model_backward.predict(
     poly_features_backward.transform(x_after_feature_selection_backward_test))
 
-print('Mean Square Error Forward Train Poly',
+print('Polynomial Regression Mean Square Error Forward Train Poly',
       metrics.mean_squared_error(np.asarray(y_train), y_train_predicted_forward))
-print('Mean Square Error Forward Test Poly', metrics.mean_squared_error(np.asarray(y_test), y_predict_test_forward))
+print('Polynomial Regression Mean Square Error Forward Test Poly', metrics.mean_squared_error(np.asarray(y_test), y_predict_test_forward))
 
-print('Mean Square Error Backward Train Poly',
+print('Polynomial Regression Mean Square Error Backward Train Poly',
       metrics.mean_squared_error(np.asarray(y_train), y_train_predicted_backward))
-print('Mean Square Error Backward Test Poly', metrics.mean_squared_error(np.asarray(y_test), y_predict_test_backward))
+print('Polynomial Regression Mean Square Error Backward Test Poly', metrics.mean_squared_error(np.asarray(y_test), y_predict_test_backward))
+
+
+# SVR
+svr = SVR(kernel='rbf')
+
+# Forward
+svr.fit(x_after_feature_selection_forward_train, y_train)
+y_train_predict_forward = svr.predict(x_after_feature_selection_forward_train)
+y_test_predict_forward = svr.predict(x_after_feature_selection_forward_test)
+
+train_mse_forward = mean_squared_error(y_train, y_train_predict_forward)
+test_mse_forward = mean_squared_error(y_test, y_test_predict_forward)
+
+print("Train MSE Forward SVR:", train_mse_forward)
+print("Test MSE Forward SVR:", test_mse_forward)
+
+# Backward
+svr.fit(x_after_feature_selection_backward_train, y_train)
+y_train_predict_backward = svr.predict(x_after_feature_selection_backward_train)
+y_test_predict_backward = svr.predict(x_after_feature_selection_backward_test)
+
+train_mse_backward = mean_squared_error(y_train, y_train_predict_backward)
+test_mse_backward = mean_squared_error(y_test, y_test_predict_backward)
+
+print("Train MSE Backward SVR:", train_mse_backward)
+print("Test MSE Backward SVR:", test_mse_backward)
+
+# Decision Tree Regression
+dt_regressor = DecisionTreeRegressor()
+
+# Forward
+dt_regressor.fit(x_after_feature_selection_forward_train, y_train)
+y_train_predict_forward_dt = dt_regressor.predict(x_after_feature_selection_forward_train)
+y_test_predict_forward_dt = dt_regressor.predict(x_after_feature_selection_forward_test)
+
+train_mse_forward_dt = mean_squared_error(y_train, y_train_predict_forward_dt)
+test_mse_forward_dt = mean_squared_error(y_test, y_test_predict_forward_dt)
+
+print("Train MSE Forward DT:", train_mse_forward_dt)
+print("Test MSE Forward DT:", test_mse_forward_dt)
+
+# Backward
+dt_regressor.fit(x_after_feature_selection_backward_train, y_train)
+y_train_predict_backward_dt = dt_regressor.predict(x_after_feature_selection_backward_train)
+y_test_predict_backward_dt = dt_regressor.predict(x_after_feature_selection_backward_test)
+
+train_mse_backward_dt = mean_squared_error(y_train, y_train_predict_backward_dt)
+test_mse_backward_dt = mean_squared_error(y_test, y_test_predict_backward_dt)
+
+print("Train MSE Backward DT:", train_mse_backward_dt)
+print("Test MSE Backward DT:", test_mse_backward_dt)
 
 
